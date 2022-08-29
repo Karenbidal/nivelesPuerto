bbc2a517c004de5ce84ee58b7fd755f9432efa22
Karenbidal <karenbidal17@gmail.com>
2022-08-28 21:37:14 -0300

    update time schedule worker

--- src/main/java/com/utn/frcu/service/ScheduleService.java
+++ src/main/java/com/utn/frcu/service/ScheduleService.java
@@ -16,7 +16,8 @@ public class ScheduleService {
     @Autowired
     private PuertoService puertoService;
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    //@Scheduled(cron = "0 0 0 * * ?") */5 * * * *
+    @Scheduled(cron = "*/5 * * * *")
     public void schedulePuertosService() throws GeneralSecurityException, IOException, ParseException {
         puertoService.guardarNivelesPuertos();
     }
